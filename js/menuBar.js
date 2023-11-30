3d7a751043e37b3c935a6504529205c6574de0f9
André Dias <andremssdias@hotmail.com>
2023-11-30 23:42:12 +0100

    Update texts, navigation bar and contact page.

--- js/menuBar.js
+++ js/menuBar.js
@@ -4,7 +4,7 @@ function addEventToMenuBarIcon() {
 
     menuBarIcon.addEventListener("click", function() {
 
-        if (menuList.style.display === "none") {
+        if (menuList.style.display === "none" || !menuList.style.display) {
             menuList.style.display = "flex";
         } else {
             menuList.style.display = "none";
